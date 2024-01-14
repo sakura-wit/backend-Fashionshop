c3eb90e18f140b04003ed5925ed3dc6b03e73252
sakura-wit <datnguyenwit@gmail.com>
2024-01-14 09:31:47 +0700

    for-deploy

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3001
+
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json())
